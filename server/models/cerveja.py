df50c0d05cc7ec7f00a143f9d33b758c88f9e40d
lucas <lcs999999999@hotmail.com>
2020-10-02 13:50:04 -0300

    :beer: alembic e modelo de cerveja

---
+++ server/models/cerveja.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Numeric
+from server.config.database import Base
+
+
+class Cerveja(Base):
+    __tablename__ = 'tb_cerveja'
+
+    id = Column(Integer, primary_key=True)
+    estilo = Column(String)
+    nome = Column(String)
+    teor_alcoolico = Column(Numeric)
+
+
+
